ca1d761a0f7934f1d562c38f6bb852ce094036f0
Richard Medina <rmedina@ruby.local>
2015-04-10 12:44:50 -1000

    production syncing

--- dlaproject/urls.py
+++ dlaproject/urls.py
@@ -30,24 +30,24 @@ urlpatterns = patterns('',
 
     # url(r'^search/$', include('haystack.urls')),
 
-    url(r'^dla/$', HomeView.as_view(), name='home'),
-    url(r'^dla/repository/info/(?P<slug>[-\w]+)/$', RepositoryView.as_view(), name='olac_repository'),
-    url(r'^dla/repository/add/$', RepositoryCreateView.as_view(), name='add_repository'),
-    url(r'^dla/repository/delete/$', RepositoryResetView.as_view(), name='delete_repository'),
-    url(r'^dla/repository/harvest/(?P<slug>[-\w]+)/$', RepositoryHarvestUpdateView.as_view(), name='harvest_repository'),
+    url(r'^/$', HomeView.as_view(), name='home'),
+    url(r'^/repository/info/(?P<slug>[-\w]+)/$', RepositoryView.as_view(), name='olac_repository'),
+    url(r'^/repository/add/$', RepositoryCreateView.as_view(), name='add_repository'),
+    url(r'^/repository/delete/$', RepositoryResetView.as_view(), name='delete_repository'),
+    url(r'^/repository/harvest/(?P<slug>[-\w]+)/$', RepositoryHarvestUpdateView.as_view(), name='harvest_repository'),
 
-    url(r'^dla/collections/update/$', CollectionsUpdateView.as_view(), name='collections_update'),
-    url(r'^dla/collections/$', CollectionListView.as_view(), name='collection_list'),
-    url(r'^dla/collection/(?P<slug>[-\w]+)/$', CollectionView.as_view(), name='collection'),
+    url(r'^/collections/update/$', CollectionsUpdateView.as_view(), name='collections_update'),
+    url(r'^/collections/$', CollectionListView.as_view(), name='collection_list'),
+    url(r'^/collection/(?P<slug>[-\w]+)/$', CollectionView.as_view(), name='collection'),
 
-    url(r'^dla/item/(?P<pk>\w+)$', ItemView.as_view(), name='item'),
+    url(r'^/item/(?P<pk>\w+)$', ItemView.as_view(), name='item'),
 
-    url(r'^dla/language/(?P<query>\w+)$', LanguageView.as_view(), name='language'),
+    url(r'^/language/(?P<query>\w+)$', LanguageView.as_view(), name='language'),
 
-    url(r'^dla/contributor/(?P<query>[-\w]+)$', ContributorView.as_view(), name='contributor'),
+    url(r'^/contributor/(?P<query>[-\w]+)$', ContributorView.as_view(), name='contributor'),
 
-    url(r'^dla/search/$', SearchView.as_view(), name='search'),
-    url(r'^dla/searchtest/$', SearchPage.as_view(), name='searchtest'),
+    url(r'^/search/$', SearchView.as_view(), name='search'),
+    url(r'^/searchtest/$', SearchPage.as_view(), name='searchtest'),
 
-    url(r'^dla/admin/', include(admin.site.urls)),
+    url(r'^/admin/', include(admin.site.urls)),
 )
